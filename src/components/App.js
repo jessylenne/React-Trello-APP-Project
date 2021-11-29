543eabeb06aab4dcd1646ebe442f4f10def65e40
Jessy Lenne <jessy.lenne@stadline.com>
2021-11-29 21:42:51 +0100

    Adapt to nico & ju

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="Header">React Trello App</div>
+        <div className="Header">Aurevoir Julien & Nico !</div>
 
         <Board />
       </div>
